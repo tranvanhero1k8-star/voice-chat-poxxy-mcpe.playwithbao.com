77c7dd6ff7ba41ab910e5fae57acc67c5c2e4ab6
tranvanhero1k8-star <tranvanhero1k8@gmail.com>
2026-03-15 22:31:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
 const http = require("http");
 const httpProxy = require("http-proxy");
+const url = require("url");
 
-const proxy = httpProxy.createProxyServer({});
+const TARGET = "http://mcpe.playwithbao.com:50938";
 
-http.createServer(function(req, res) {
-  proxy.web(req, res, {
-    target: "http://mcpe.playwithbao.com:50938",
-    changeOrigin: true
+let players = new Map();
+
+const proxy = httpProxy.createProxyServer({
+  target: TARGET,
+  ws: true,
+  changeOrigin: true
+});
+
+function showPlayers() {
+  const list = [...players.values()];
+  console.log("👥 Voice online (" + list.length + "):", list.join(", "));
+}
+
+const server = http.createServer((req, res) => {
+  proxy.web(req, res);
+});
+
+server.on("upgrade", (req, socket, head) => {
+
+  const query = url.parse(req.url, true).query;
+  const username = query.username || "Unknown";
+
+  players.set(socket, username);
+
+  console.log("🎤 JOIN:", username);
+  showPlayers();
+
+  socket.on("close", () => {
+    const name = players.get(socket);
+    players.delete(socket);
+
+    console.log("❌ LEAVE:", name);
+    showPlayers();
   });
-}).listen(process.env.PORT || 10000);
+
+  proxy.ws(req, socket, head);
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Voice proxy running");
+});
